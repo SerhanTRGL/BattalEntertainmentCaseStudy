577e36b64591ba5418f800c7bad939a3d899c70e
SerhanT <serhanturgul2@gmail.com>
2026-04-28 02:09:35 +0300

    Add coin price UI & update BuildingSelectorItem
    Add UI elements for displaying resource prices (Price, Prices, CoinPrice, Icon, ResourcesCanvas) in the BuildingSelectorItem prefab and UIScene. Implement BuildingSelectorItem.cs to show the coin build cost, subscribe to PlayerResources.OnResourceValueChanged, and tint the coin price red when the player lacks sufficient coins. Update TextMeshPro material settings (outline width and related properties). Add new Visuals assets (coin/xp sprites and metadata) and a Visuals folder meta file.

--- Assets/Scripts/BuildingSelectorItem.cs
+++ Assets/Scripts/BuildingSelectorItem.cs
@@ -1,7 +1,23 @@
+using System;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BuildingSelectorItem : Selectable
 {
-    
+    [SerializeField] private GridEntity building;
+    [SerializeField] private TextMeshProUGUI coinText;
+    private float coinPrice;
+    protected override void Awake() {
+        PlayerResources.OnResourceValueChanged += UpdateItemVisual;
+        coinPrice = building.buildCost.Where(t => t.Type == ResourceType.Coin).First().Amount;
+        coinText.text = coinPrice.ToString();
+    }
+
+    private void UpdateItemVisual(ResourceChange change) {
+        if (change.changedResource.Type != ResourceType.Coin) return;
+
+        coinText.color = change.changedResource.Amount < coinPrice ? Color.red : Color.white;
+    }
 }
